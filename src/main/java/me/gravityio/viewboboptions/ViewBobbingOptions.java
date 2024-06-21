9106c27243465ade40efaf4020d29d7cb3a8cb1e
Harry <harryioannoucy@gmail.com>
2024-06-21 21:20:54 +0300

    cherry pick 1.20.3

--- src/main/java/me/gravityio/viewboboptions/ViewBobbingOptions.java
+++ src/main/java/me/gravityio/viewboboptions/ViewBobbingOptions.java
@@ -1,10 +1,8 @@
 package me.gravityio.viewboboptions;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.item.ItemStack;
@@ -14,7 +12,7 @@ import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ViewBobbingOptions implements ClientModInitializer, PreLaunchEntrypoint {
+public class ViewBobbingOptions implements ClientModInitializer {
     public static final String MOD_ID = "viewboboptions";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     private static final KeyBinding ADD_ITEM_BIND = new KeyBinding("viewboboptions.add_item_bind", GLFW.GLFW_KEY_Z, "category.gravityio.name");
@@ -30,14 +28,9 @@ public class ViewBobbingOptions implements ClientModInitializer, PreLaunchEntryp
     }
 
     @Override
-    public void onPreLaunch() {
-        MixinExtrasBootstrap.init();
+    public void onInitializeClient() {
         ModConfig.GSON.load();
         ModConfig.INSTANCE = ModConfig.GSON.instance();
-    }
-
-    @Override
-    public void onInitializeClient() {
         KeyBindingHelper.registerKeyBinding(ADD_ITEM_BIND);
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
