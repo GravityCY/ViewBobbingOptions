112782f2cd699e89d4b205360a1a59ae26d318e8
GravityIO <Harryioannoucy@gmail.com>
2024-08-12 23:56:29 +0300

    Revert "1.20, 1.20.1"
    This reverts commit 170526f4c164f18bf545ab24c9640b10f24d36f1.

--- src/main/java/me/gravityio/viewboboptions/mixin/compat/IrisHandRendererMixin.java
+++ src/main/java/me/gravityio/viewboboptions/mixin/compat/IrisHandRendererMixin.java
@@ -3,6 +3,7 @@ package me.gravityio.viewboboptions.mixin.compat;
 import com.mojang.blaze3d.vertex.PoseStack;
 import me.gravityio.viewboboptions.mixin.TransientMixinData;
 import me.gravityio.viewboboptions.mixin.TransientMixinData.BobType;
+import net.irisshaders.iris.pathways.HandRenderer;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,23 +12,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
 //? if >=1.20.5 {
-/*import org.joml.Matrix4fc;
+import org.joml.Matrix4fc;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-*///?} elif =1.20.3 || =1.20.1 {
-/*import net.irisshaders.iris.pathways.HandRenderer;
-*///?} else {
-import net.coderbot.iris.pipeline.HandRenderer;
-//?}
-
-//? if <=1.20.3 {
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//?}
+//?} else {
+/*import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+*///?}
 
 @Pseudo
 @Mixin(HandRenderer.class)
 public class IrisHandRendererMixin {
     //? if >=1.20.5 {
-    /*@Inject(method = "setupGlState", at = @At("HEAD"))
+    @Inject(method = "setupGlState", at = @At("HEAD"))
     private void setHandBobMode(GameRenderer gameRenderer, Camera camera, Matrix4fc modelMatrix, float tickDelta, CallbackInfoReturnable<PoseStack> cir) {
         TransientMixinData.CURRENT = BobType.HAND;
     }
@@ -36,8 +31,8 @@ public class IrisHandRendererMixin {
     private void setNoneBobMode(GameRenderer gameRenderer, Camera camera, Matrix4fc modelMatrix, float tickDelta, CallbackInfoReturnable<PoseStack> cir) {
         TransientMixinData.CURRENT = BobType.NONE;
     }
-    *///?} else {
-    @Inject(method = "setupGlState", at = @At("HEAD"))
+    //?} else {
+    /*@Inject(method = "setupGlState", at = @At("HEAD"))
     private void setHandBobMode(GameRenderer gameRenderer, Camera camera, PoseStack poseStack, float tickDelta, CallbackInfo ci) {
         TransientMixinData.CURRENT = BobType.HAND;
     }
@@ -46,5 +41,5 @@ public class IrisHandRendererMixin {
     private void setNoneBobMode(GameRenderer gameRenderer, Camera camera, PoseStack poseStack, float tickDelta, CallbackInfo ci) {
         TransientMixinData.CURRENT = BobType.NONE;
     }
-    //?}
+    *///?}
 }
