108e4796bb9d7efd14bdf355bc9c2450cb8664d2
GravityIO <Harryioannoucy@gmail.com>
2025-01-03 12:44:50 +0200

    fixed naming for old versions

--- src/main/java/me/gravityio/viewboboptions/ViewBobbingOptions.java
+++ src/main/java/me/gravityio/viewboboptions/ViewBobbingOptions.java
@@ -50,15 +50,22 @@ public class ViewBobbingOptions implements ClientModInitializer {
         VanillaOptions.init();
     }
 
+    private Component getItemName(ItemStack stack) {
+        //? if >=1.21.2 {
+        return stack.getItemName();
+        //?} else {
+        /*return stack.getHoverName();        *///?}
+    }
+
     private void onAddItem(LocalPlayer player) {
         for (ItemStack handItem : player.getHandSlots()) {
             if (handItem.isEmpty()) continue;
             String id = BuiltInRegistries.ITEM.getKey(handItem.getItem()).toString();
             if (ModConfig.INSTANCE.stationary_items.contains(id)) {
-                player.displayClientMessage(Component.translatable("messages.viewboboptions.remove_item", handItem.getItemName()), true);
+                player.displayClientMessage(Component.translatable("messages.viewboboptions.remove_item", this.getItemName(handItem)), true);
                 ModConfig.INSTANCE.stationary_items.remove(id);
             } else {
-                player.displayClientMessage(Component.translatable("messages.viewboboptions.add_item", handItem.getItemName()), true);
+                player.displayClientMessage(Component.translatable("messages.viewboboptions.add_item", this.getItemName(handItem)), true);
                 ModConfig.INSTANCE.stationary_items.add(id);
             }
             ModConfig.GSON.save();
